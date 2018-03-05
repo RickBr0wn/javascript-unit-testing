f7ce3a43288f7a5edd71afce671e5457e2b64c6a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-05 18:32:46 +0000

    Added non-capitilization of 'connecting' words

--- test/textUtilities.js
+++ test/textUtilities.js
@@ -1,16 +1,23 @@
 var expect = require('chai').expect;
 
 function titleCase(title){
+    let connectionWords = ["of", "and", "from", "in", "on", "a", "the"];
     let words = title.split(' ');
-    let titleCaseWords = words.map(function(word){
-        return word[0].toUpperCase() + word.substring(1);
+    let titleCaseWords = words.map(function(word, index){
+        if(connectionWords.includes(word.toLowerCase())
+        && index != 0)
+        {
+            return word.toLowerCase();
+        }else{
+            return word[0].toUpperCase() + word.substring(1).toLowerCase();
+        }
+        
     });
-
     return titleCaseWords.join(' ');
 }
 
-expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('the towers of the city of london')).to.be.a('string');
 expect(titleCase('a')).to.equal('A');
 expect(titleCase('vertical')).to.equal('Vertical');
 
-expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+expect(titleCase('the towers of the city of london')).to.equal('The Towers of the City of London');
